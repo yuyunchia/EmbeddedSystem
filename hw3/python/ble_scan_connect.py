1b9ea95f6db7cd780776467276218d7418efaf0c
傅譽的MacBook <b08901039@ntu.edu.tw>
2021-10-28 12:14:14 +0800

    try ble with bluepy

--- hw3/python/ble_scan_connect.py
+++ hw3/python/ble_scan_connect.py
@@ -1,65 +1,52 @@
-from bluepy.btle import Peripheral, UUID, ADDR_TYPE_PUBLIC, ADDR_TYPE_RANDOM
-from bluepy.btle import Scanner, DefaultDelegate
-
+from bluepy.btle import Peripheral, DefaultDelegate
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device", dev.addr)
-        elif isNewData:
-            print("Received new data from", dev.addr)
+    def handleDiscovery(self, cHandle, data):
+        print(f"Data from handle {cHandle}: {data}")
 
-# scanner = Scanner().withDelegate(ScanDelegate())
-# scanner.clear()
+def main():
+    print("Connecting...")
+    dev = Peripheral('a8:9c:ed:a0:5e:fb', 'public')
+    dev.setDelegate(ScanDelegate())
 
-# devices = scanner.scan(10.0)
-# n=0
-# for dev in devices:
-#     print ("%d: Device %s (%s), RSSI=%d dB") % (n, dev.addr, dev.addrType, dev.rssi)
-#     n += 1
-#     for (adtype, desc, value) in dev.getScanData():
-#         print ("  %s = %s") % (desc, value)
-# number = input('Enter your device number: ')
+    print("Services...")
+    for svc in dev.services:
+        print(str(svc.uuid))
 
-# mappedAddrType = ''
+    try:
+        # while True:
+        descriptor = dev.getDescriptors()
+        print("Num of discriptors: %d" % (len(descriptor)))
+        print("Descriptors: ")
+        for d in descriptor:
+            print("handle: %s, uuid: %s" % (str(d.handle), str(d.uuid)))
+            if "2902" in str(d.uuid):
+                print("Writing to uuid: %s" % (str(d.uuid)))
+                # print(dev.writeCharacteristic(d.handle, b"\x01\x00", withResponse=True))
+                print(d.write(b"\x01\x00", withResponse=True))
 
-# print('Device', number)
-# print(devices[number].addr)
+        while True:
+            if dev.waitForNotifications(1.0):
+                continue
+            print("Waiting...")
 
-# if (addr_type == 'public') :
-#     mappedAddrType = ADDR_TYPE_PUBLIC
+        # service = dev.getServiceByUUID(0xfff0)
+        # charas = service.getCharacteristics()
+        # print("Num of characteristics: ", len(charas))
+        # print("Characteristics: ")
+        # for c in charas:
+        #     print("uuid: %s, property: %s" % (str(c.uuid), c.propertiesToString()))
+        #     print(c.read())
+        # for c in charas:
+        #     if "NOTIFY" in c.propertiesToString():
+        #         print("characteristic with uuid %s is not readible" % (str(c.uuid)))
+                # print(dev.handleNotification(c.getHandle(), c.read()))
 
-# if (addr_type == 'random') :
-#     mappedAddrType = ADDR_TYPE_RANDOM
+    finally:
+        dev.disconnect()
 
-
-print("Connecting...")
-dev = Peripheral('a8:9c:ed:a0:5e:fb', 'public')
-
-print("Services...")
-for svc in dev.services:
-    print(str(svc))
-
-try:
-    testService = dev.getServiceByUUID(UUID(0xfff0))
-    print(testService.getCharacteristics())
-    for ch in testService.getCharacteristics():
-        print(str(ch))
-    # for ds in testService.getDescriptors():
-    #     print (str(ds))
-
-    ch = dev.getCharacteristics(uuid=UUID(0xfff2))[0]
-    print(str(ch))
-    print(str(ch.propertiesToString()))
-    dev.writeCharacteristic(ch.getHandle()+1, "\x01\x00".encode())
-    print('dev: ', dev)
-    ch = dev.getCharacteristics(uuid=UUID(0xfff2))[0]
-    print(ch)
-    if (ch.supportsRead()):
-        print('puppy', ch.read())
-
-finally:
-    dev.disconnect()
+if __name__ == "__main__":
+    main()
