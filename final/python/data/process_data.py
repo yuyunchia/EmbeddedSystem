415eebefaf5d27f3962c847cfd52ee6e4853bb87
傅譽的MacBook <b08901039@ntu.edu.tw>
2021-12-17 16:04:11 +0800

    add training code

---
+++ final/python/data/process_data.py
@@ -0,0 +1,31 @@
+import os
+import csv
+
+config = {
+    "total_data_dir": "../../data/total_data.csv",
+    "merge_path": "../../data"
+}
+
+if __name__ == "__main__":
+    with open(config["total_data_dir"], "w") as csv_file:
+        tag = ["motion", "label"]
+        writer = csv.DictWriter(csv_file, fieldnames=tag, quoting=csv.QUOTE_NONE)
+        writer.writeheader()
+
+        merge_files = os.listdir(config["merge_path"])
+        for file in merge_files:
+            if ".txt" in file:
+                file_path = os.path.join(config["merge_path"], file)
+                print("Merging %s" % file_path)
+                with open(file_path, "r") as txt_file:
+                    lines = txt_file.readlines()
+                    for l in lines:
+                        _l = l.split()
+                        data = _l[: 24]
+                        label = _l[-1]
+                        writer.writerow({
+                            "motion": " ".join(data),
+                            "label": label
+                        })
+                    txt_file.close()
+        csv_file.close()
---
+++ final/python/ml/train_motion.py
@@ -0,0 +1,66 @@
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import os
+
+from motionnet import MotionNet
+from motionset import MotionSet
+
+config = {
+    "data_dir": "../../data/total_data.csv",
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+    "batch_size": 4,
+    "lr": 1e-5,
+    "weight_decay": 5e-6,
+    "epoch": 1000,
+    "model_save_dir": "../../models",
+    "loss_fn": torch.nn.CrossEntropyLoss()
+}
+
+def train():
+    train_set = MotionSet(config["data_dir"])
+    print("Train set length: %d" % len(train_set))
+    train_loader = DataLoader(train_set, batch_size=config["batch_size"], shuffle=True)
+
+    device = config["device"]
+    print("Using device: %s" % device)
+
+    model = MotionNet()
+    model.to(device)
+
+    epoch = config["epoch"]
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"])
+    loss_fn = config["loss_fn"]
+
+
+    best_acc = 0.0
+    model.train()
+    for e in range(epoch):
+        train_loss, train_acc = [], []
+        for batch in tqdm(train_loader):
+            motion, label = batch
+            motion, label = motion.to(device), label.to(device)
+
+            output = model(motion)
+            
+            loss = loss_fn(output, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            acc = (output.argmax(dim=-1) == label).float().mean()
+            train_loss.append(loss.item())
+            train_acc.append(acc.item())
+        
+        train_loss = sum(train_loss) / len(train_loss)
+        train_acc = sum(train_acc) / len(train_acc)
+        if best_acc < train_acc:
+            best_acc = train_acc
+            save_path = os.path.join(config["model_save_dir"], "motion_%d.ckpt" % int(best_acc * 100))
+            print("Saving model at %s" % save_path)
+            torch.save(model.state_dict(), save_path)
+        
+        print(f"[Overall {e + 1:04d}/{epoch:04d}] train loss: {train_loss:.5f}, train acc: {train_acc:.5f}, best train acc:{best_acc:.5f}")
+
+if __name__ == "__main__":
+    train()
