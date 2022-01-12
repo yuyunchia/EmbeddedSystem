0e575f60578b95d7dfabdcea1b6431a64b612f96
傅譽的MacBook <b08901039@ntu.edu.tw>
2022-01-12 14:12:09 +0800

    data collection socket

--- final/python/data/get_data_socket.py
+++ final/python/data/get_data_socket.py
@@ -5,7 +5,7 @@ import netifaces as ni
 def main():
     # bind_ip = "192.168.43.215"
     bind_ip = ni.ifaddresses('en0')[ni.AF_INET][0]['addr']
-    bind_port = 4000
+    bind_port = 3000
     socket_available = 10
     buffer_size = 2048
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,7 +14,8 @@ def main():
     server.listen(socket_available)
     print("Listening on %s:%d, with maximum socket available %d" % (bind_ip, bind_port, socket_available))
 
-    with open("../../data/4_data.txt", "w") as data_file:
+    with open("../../data/raw_test.txt", "w") as data_file:
+        total_data = []
         while True:
             print("Server ready")
             client, addr = server.accept()
@@ -25,14 +26,9 @@ def main():
                 try:
                     data = str(client.recv(buffer_size), encoding='utf-8')
                     print("Data from client: ", data)
-                    choise = input("Choise [l/r/n/s]:")
-                    if choise == "close":
-                        client.close()
-                        break
-                    if choise not in ['l', 'r', 'n', 's']:
-                        print("Don't have the choise: %s" % choise)
 		            
-                    data_file.write("%s -> %s\n" % (data, choise))
+                    data_file.write("%s\n" % data)
+                    # data_file.write("%s -> %s\n" % (data, choise))
                 except UnicodeDecodeError:
                     print("Data format error")
                     client.close()
