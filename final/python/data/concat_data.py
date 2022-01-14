818172ef67e9d3ac0fb1e0adc77743e094f7ace7
傅譽的MacBook <b08901039@ntu.edu.tw>
2022-01-14 16:22:32 +0800

    more data

---
+++ final/python/data/concat_data.py
@@ -0,0 +1,46 @@
+import os
+import csv
+
+config = {
+    "total_data_dir": "../../data/total_data.csv",
+    "merge_path": "../../data"
+}
+
+if __name__ == "__main__":
+    with open(config["total_data_dir"], "w") as csv_file:
+        tag = ["motion", "label"]
+        writer = csv.DictWriter(csv_file, fieldnames=tag, quoting=csv.QUOTE_NONE)
+        writer.writeheader()
+
+        merge_files = sorted(os.listdir(config["merge_path"]))
+        for file in merge_files:
+            if (".txt" in file) and ("test" not in file):
+                file_path = os.path.join(config["merge_path"], file)
+                print("Merging %s" % file_path)
+                with open(file_path, "r") as txt_file:
+                    lines = txt_file.readlines()
+                    for l in lines:
+                        _l = l.split()
+                        if len(_l) > 26:
+                            continue
+                        if len(_l) == 26:
+                            data = _l[: 24]
+                            label = _l[-1]
+                            if label in "rlsn":
+                                writer.writerow({
+                                    "motion": " ".join(data),
+                                    "label": label
+                                })
+                        if len(_l) == 24:
+                            if "none" in file:
+                                label = 'n'
+                            elif "right" in file:
+                                label = 'r'
+                            elif "left" in file:
+                                label = 'l'
+                            writer.writerow({
+                                "motion": " ".join(_l),
+                                "label": label
+                            })
+                    txt_file.close()
+        csv_file.close()
